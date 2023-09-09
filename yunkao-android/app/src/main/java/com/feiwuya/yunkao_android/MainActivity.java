0c840bf6ccd8b26be0cb9a53112db09fc7ddb2c2
feiwuya <feiwuya@outlook.com>
2023-09-10 02:43:00 +0800

    push

--- yunkao-android/app/src/main/java/com/feiwuya/yunkao_android/MainActivity.java
+++ yunkao-android/app/src/main/java/com/feiwuya/yunkao_android/MainActivity.java
@@ -1,11 +1,8 @@
 package com.feiwuya.yunkao_android;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
 
 import com.feiwuya.yunkao_android.frame.HomeFragment;
 import com.feiwuya.yunkao_android.frame.MyFragment;
@@ -28,19 +25,25 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottom_nav);
         // 解决导航过多只显示图片问题
         bottomNavigationView.setLabelVisibilityMode(NavigationBarView.LABEL_VISIBILITY_LABELED);
-        // 事件绑定
+        /**
+         * 底部导航栏点击事件
+         */
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int selectedId = item.getItemId();
             if (selectedId == R.id.nav_home) {
+                // 主页
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
                 return true;
             } else if (selectedId == R.id.nav_practice) {
+                // 刷题页
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new PracticeFragment()).commit();
                 return true;
             } else if (selectedId == R.id.nav_quick_cal) {
+                // 速算练习页
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new QuickCalFragment()).commit();
                 return true;
             } else if (selectedId == R.id.nav_my) {
+                // 我的信息页
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MyFragment()).commit();
                 return true;
             }
--- yunkao-android/app/src/main/java/com/feiwuya/yunkao_android/frame/HomeFragment.java
+++ yunkao-android/app/src/main/java/com/feiwuya/yunkao_android/frame/HomeFragment.java
@@ -2,13 +2,25 @@ package com.feiwuya.yunkao_android.frame;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.feiwuya.yunkao_android.R;
+import com.youth.banner.Banner;
+import com.youth.banner.adapter.BannerImageAdapter;
+import com.youth.banner.holder.BannerImageHolder;
+import com.youth.banner.indicator.CircleIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,34 +28,15 @@ import com.feiwuya.yunkao_android.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Banner homeBanner;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,10 +44,8 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
@@ -62,5 +53,25 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        homeBanner = getView().findViewById(R.id.home_banner);
+        List<String> bannerList = new ArrayList<>();
+        bannerList.add("https://p3.ssl.qhimgs1.com/t017e6730377e3c2003.jpg");
+        bannerList.add("https://p1.ssl.qhimgs1.com/sdr/400__/t01da8cfe1feec230cb.jpg");
+        bannerList.add("http://photocdn.sohu.com/20110722/Img314235964.jpg");
+        bannerList.add("https://wallpaperm.cmcm.com/741908106926470dd8a298a6fda66189.jpg");
+        homeBanner.setAdapter(new BannerImageAdapter<String>(bannerList) {
+            @Override
+            public void onBindView(BannerImageHolder holder, String data, int position, int size) {
+                Glide.with(holder.itemView)
+                        .load(data)
+                        .apply(RequestOptions.bitmapTransform(new RoundedCorners(30)))
+                        .into(holder.imageView);
+            }
+        }).addBannerLifecycleObserver(this).setBannerRound(30).setIndicator(new CircleIndicator(getContext()));
     }
 }
