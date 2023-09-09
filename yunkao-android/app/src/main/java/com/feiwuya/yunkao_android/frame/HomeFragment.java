6686e2bb546d4379d3b3e411d30d9e4d5bd8b03f
feiwuya <feiwuya@outlook.com>
2023-09-10 03:22:53 +0800

    push

--- yunkao-android/app/src/main/java/com/feiwuya/yunkao_android/frame/HomeFragment.java
+++ yunkao-android/app/src/main/java/com/feiwuya/yunkao_android/frame/HomeFragment.java
@@ -49,8 +49,7 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
@@ -60,17 +59,15 @@ public class HomeFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         homeBanner = getView().findViewById(R.id.home_banner);
         List<String> bannerList = new ArrayList<>();
+        // :TODO 轮播图设置
         bannerList.add("https://p3.ssl.qhimgs1.com/t017e6730377e3c2003.jpg");
         bannerList.add("https://p1.ssl.qhimgs1.com/sdr/400__/t01da8cfe1feec230cb.jpg");
-        bannerList.add("http://photocdn.sohu.com/20110722/Img314235964.jpg");
+        bannerList.add("https://wallpaperm.cmcm.com/5209f7770b558faf5dccf56e416eb14e.jpg");
         bannerList.add("https://wallpaperm.cmcm.com/741908106926470dd8a298a6fda66189.jpg");
         homeBanner.setAdapter(new BannerImageAdapter<String>(bannerList) {
             @Override
             public void onBindView(BannerImageHolder holder, String data, int position, int size) {
-                Glide.with(holder.itemView)
-                        .load(data)
-                        .apply(RequestOptions.bitmapTransform(new RoundedCorners(30)))
-                        .into(holder.imageView);
+                Glide.with(holder.itemView).load(data).apply(RequestOptions.bitmapTransform(new RoundedCorners(30))).into(holder.imageView);
             }
         }).addBannerLifecycleObserver(this).setBannerRound(30).setIndicator(new CircleIndicator(getContext()));
     }
