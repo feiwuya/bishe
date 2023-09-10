2f175e78b1c3c6fe11541eda167dff9943357b88
feiwuya <feiwuya@outlook.com>
2023-09-11 03:53:06 +0800

    default

---
+++ yunkao-server/src/test/java/com/feiwuya/yunkaoserver/YunkaoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feiwuya.yunkaoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunkaoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
