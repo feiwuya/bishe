75add8da56b4747693a12d7c402cee97d7eace0c
feiwuya <feiwuya@outlook.com>
2023-09-13 20:45:31 +0800

    create server project

---
+++ yunkao-server/server-android/src/test/java/com/feiwuya/serverandroid/ServerAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feiwuya.serverandroid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAndroidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ yunkao-server/server-web/src/test/java/com/feiwuya/serverweb/ServerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feiwuya.serverweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
