679af862766dad7f6a36a58b7d301eb71bf1215a
feiwuya <feiwuya@outlook.com>
2023-09-18 02:42:04 +0800

    create server project

---
+++ yunkao-server/src/main/java/com/feiwuya/yunkaoserver/YunkaoServerApplication.java
@@ -0,0 +1,13 @@
+package com.feiwuya.yunkaoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunkaoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunkaoServerApplication.class, args);
+    }
+
+}
